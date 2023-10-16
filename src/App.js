689a3cbbbda7f77d67f6e812e1e9f0a988465614
VKU CODE <ungurean729@gmail.com>
2023-10-16 12:20:36 +0200

    update data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Accueil from "./components/Accueil";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/accueil" element={<Accueil />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
